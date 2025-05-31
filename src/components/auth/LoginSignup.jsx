efa3236c5c1625e6c842d887c290e73f1d7e1f1c
zshaan25 <154325262+zshaan25@users.noreply.github.com>
2025-05-31 19:12:09 +0500

    Add files via upload

--- src/components/auth/LoginSignup.jsx
+++ src/components/auth/LoginSignup.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
-import "../pages/LoginSignup.css"
-const LoginSignup = () => {
+import "../../components/auth/LoginSignup.css"
+function LoginSignup() {
   return (
     <div className='loginsignup'>
       <div className="loginsignup-container">
         <h1>Sign Up</h1>
         <div className='loginsignup-fields'>
-          <input type='text' placeholder='Your Name'/>
-          <input type='email' placeholder='Email Address'/>
-          <input type='password' placeholder='Password'/>
+          <input type='text' placeholder='Your Name' />
+          <input type='email' placeholder='Email Address' />
+          <input type='password' placeholder='Password' />
           <button>Continue</button>
         </div>
-        
+
         <p className="loginsignup-login">
           Already have an account ? <span>Login here</span>
         </p>
         <div className='loginsignup-agree'>
-          <input type='checkbox' name='' id=''/>
+          <input type='checkbox' name='' id='' />
           <p>By continuing, i agree to the terms of use & privacy policy.</p>
         </div>
       </div>
